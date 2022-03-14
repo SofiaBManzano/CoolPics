ec9c5802f63e51842c0aae2fece09090d8f1d733
SofiaBManzano <sofiabmanzano@gmail.com>
2022-03-14 20:33:41 +0100

    wip hover images

--- src/components/Main/Image.js
+++ src/components/Main/Image.js
@@ -1,16 +1,18 @@
 import Number from "./Number";
 const Image = (props) => {
   return (
-    <div
-      style={{ backgroundImage: `url('${props.eachImage.download_url}')` }}
-      className="list__img"
-    >
-      <Number index={props.index} />
-      {/* <img
+    <div className="list__img wrapper">
+      <div
+        style={{ backgroundImage: `url('${props.eachImage.download_url}')` }}
+        className="list__img item"
+      >
+        <Number index={props.index} />
+        {/* <img
       className="list__container--img"
       src={props.eachImage.download_url}
      alt={`Author: ${props.eachImage.author}`}
      /> */}
+      </div>
     </div>
   );
 };
